b3fea23b22d0ca5bd8629b17ecb818f432de10db
Evan <ekhosh60@stuy.edu>
2024-09-27 11:40:16 -0400

    Wrote first version of returnCopy function

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -2,14 +2,23 @@ public class MyArrays {
   public static void main(String[] args) {
     System.out.println(arrayToString(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9}));
   }
-  public static String arrayToString(int[] nums) {
+
+  public static String arrayToString(int[] ary) {
     String out = "[";
-    for (int i = 0; i < nums.length; i++) {
-      out += nums[i];
-      if (i != nums.length - 1) {
+    for (int i = 0; i < ary.length; i++) {
+      out += ary[i];
+      if (i != ary.length - 1) {
         out += ", ";
       }
     }
     return out + "]";
+  }
+
+  public static int[] returnCopy(int[] ary) {
+    int[] copy = new int[ary.length];
+    for (int i = 0; i < ary.length; i++) {
+      copy[i] = ary[i];
+    }
+    return copy;
   }
 }
