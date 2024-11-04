a8b2c17c70372326401fb66588fe0d95cbd3837b
Evan <ekhosh60@stuy.edu>
2024-11-04 11:37:26 -0500

    Added finished work done on partner's computer.

--- 10-31-Advent/Driver.java
+++ 10-31-Advent/Driver.java
@@ -2,14 +2,81 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class Driver{
-
+public class TriangleTester{
   public static void main(String[] args) {
+    System.out.println("countTriangleA: "+countTrainglesA("inputTri.txt"));
+    System.out.println("countTriangleB: "+countTrainglesB("inputTri.txt"));
   }
 
-  public static boolean isTriangle(String input){
+  public static int countTrainglesA(String filename){
+    int count=0;
+    try {
+      File file = new File(filename);
+      Scanner input = new Scanner(file);
+      while(input.hasNextInt()){
+        if(isTriangle(lineToIntArray(input.nextLine()))) {
+            count++;
+        }
+      }
+      input.close();
+      return count;
+    } catch (FileNotFoundException ex) {
+      System.out.println("File not found");
+      return -1;
+    }
+  }
+
+  public static int countTrainglesB(String filename){
+    int count=0;
+    int[][] arr = new int[3][3];
+    try {
+      File file = new File(filename);
+      Scanner input = new Scanner(file);
+      while(input.hasNextInt()){
+        for (int i=0; i<3; i++) {
+          arr[i]=lineToIntArray(input.nextLine());
+        }
+        count+=count3TrianglesB(arr);
+      }
+      input.close();
+      return count;
+    } catch (FileNotFoundException ex) {
+      System.out.println("File not found");
+      return -1;
+    }
+  }
+
+  public static int[] lineToIntArray(String input) {
     Scanner sc = new Scanner(input);
-    s0 = sc.nextInt(); s1 = sc.nextInt(); s2 = sc.nextInt();
-    return (s0 + s1 > s2 && s1 + s2 > s0 && s2 + s0 > s2);
+    int[] s=new int[3];
+    for (int i=0; i<3; i++) {
+      s[i] = sc.nextInt();
+    }
+    return s;
+  }
+
+  public static boolean isTriangle(int[] s){
+    return (s[0] + s[1] > s[2] && s[1] + s[2] > s[0] && s[2] + s[0] > s[1]);
+  }
+
+  public static int[][] swapRC(int[][]nums){
+    int[][] ans= new int[nums[0].length][nums.length];
+    for (int i=0; i<nums[0].length; i++) {
+      for (int j=0; j<nums.length; j++) {
+          ans[i][j]=nums[j][i];
+      }
+    }
+    return ans;
+  }
+
+  public static int count3TrianglesB(int[][] arr) {
+    arr = swapRC(arr);
+    int count=0;
+    for (int i=0; i<3; i++) {
+      if (isTriangle(arr[i])) {
+        count++;
+      }
+    }
+    return count;
   }
 }
