82e8c71dc506f18cc6f06d3766d07c015af27840
Evan Khosh <evank43@nycstudents.net>
2024-11-20 20:18:10 -0500

    Wrote final parseFile method for Dat 4

--- 2024-11-18-advent2016/Day4.java
+++ 2024-11-18-advent2016/Day4.java
@@ -1,18 +1,24 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Day4 {
-  private static String[] parseFile(String filename){
+  private static String[][] parseFile(String filename){
     try {
       File file = new File(filename);
       Scanner input = new Scanner(file);
-      String data = input.next("-");
-      while (input.hasNext()){
-        data += ", " + input.next("-");
+      String data = input.nextLine();
+      while (input.hasNextLine()){
+        data += ", " + input.nextLine();
       }
       input.close();
-      return data.split(", ");
+      String[] dataArr = data.split(", ");
+      String[][] dataArrArr = new String[dataArr.length][];
+      for (int i = 0; i < dataArr.length; i++){
+        dataArrArr[i] = dataArr[i].split("-");
+      }
+      return dataArrArr;
     } catch (FileNotFoundException ex) {
       System.out.println("File not found");
       return null;
@@ -20,6 +26,8 @@ public class Day4 {
   }
 
   public static int sumRealIDs(String filename){
-    String[] data = parseFile(filename);
+    String[][] data = parseFile(filename);
+    System.out.println(Arrays.deepToString(data));
+    return 0;
   }
 }
--- 2024-11-18-advent2016/Driver.java
+++ 2024-11-18-advent2016/Driver.java
@@ -3,8 +3,11 @@ public class Driver {
     //System.out.println("Day 1\n");
     //System.out.println("Part 1: " + Day1.distance("ReadFile.txt"));
 
-    System.out.println("\nDay 2\n");
-    System.out.println("Part 1: " + Day2.bathroomCode("ReadFile.txt"));
-    System.out.println("Part 2: " + Day2.bathroomCodeDia("ReadFile.txt"));
+    //System.out.println("\nDay 2\n");
+    //System.out.println("Part 1: " + Day2.bathroomCode("ReadFile.txt"));
+    //System.out.println("Part 2: " + Day2.bathroomCodeDia("ReadFile.txt"));
+
+    System.out.println("\nDay 4\n");
+    System.out.println("Part 1: " + Day4.sumRealIDs("ReadFile.txt"));
   }
 }
