b4786d4180e5a9591234fe22279d18ebd159eb4e
Evan <ekhosh60@stuy.edu>
2024-12-03 11:58:13 -0500

    Wrote color function to change foreground and background and color function that can take modifiers.

--- 2024-12-03-colors/ColorDemo.java
+++ 2024-12-03-colors/ColorDemo.java
@@ -2,4 +2,23 @@ public class ColorDemo {
   public static void main(String[] args){
 
   }
+
+  public static void color(int foreground, int background){
+    System.out.print( "\u001b[" + foreground + ";" + (10+background) + "m");
+  }
+
+  public static void color(int foreground, int background, int[] modifiers){
+    String mods = "";
+    for (int i = 0; i < modifiers.length; i++){
+      mods += ";" + modifiers[i];
+    }
+    System.out.print( "\u001b[" + foreground + ";" + (10+background) + mods + "m");
+  }
+
+  public static void sleep(int milli){
+    try{
+      Thread.sleep(milli);
+    }catch(Exception e){
+    }
+  }
 }
