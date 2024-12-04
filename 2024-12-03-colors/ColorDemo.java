aa1ee11381246053958dd405b84b8c77e2c84d16
Evan Khosh <evank43@nycstudents.net>
2024-12-03 20:54:56 -0500

    Wrote code to make diagonal rainbow waves move across the terminal using colored periods.

--- 2024-12-03-colors/ColorDemo.java
+++ 2024-12-03-colors/ColorDemo.java
@@ -1,10 +1,22 @@
 public class ColorDemo {
-  public static void main(String[] args){
+  public static String BLACK = "30";
 
+  public static void main(String[] args){
+    String[] colors = new String[]{"48;2;255;0;0;7", "48;2;255;127;0;7", "48;2;255;255;0;7", "48;2;127;255;0;7", "48;2;0;255;0;7", "48;2;0;255;127;7", "48;2;0;255;255;7", "48;2;0;127;255;7", "48;2;0;0;255;7", "48;2;127;0;255;7", "48;2;255;0;255;7", "48;2;255;0;127;7"};
+    for (int i = 0; i < 100; i++){
+      for (int k = 0; k < colors.length; k++){
+        for (int j = 0; j < colors.length; j++){
+          color(BLACK, colors[(j+k)%colors.length]);
+          System.out.print(".");
+        }
+      }
+      sleep(50);
+    }
+    System.out.print("\u001b[0m");
   }
 
-  public static void color(int foreground, int background){
-    System.out.print( "\u001b[" + foreground + ";" + (10+background) + "m");
+  public static void color(String foreground, String background){
+    System.out.print( "\u001b[" + foreground + ";" + background + "m");
   }
 
   public static void color(int foreground, int background, int[] modifiers){
