9453a2114c281fac0cb407ed098ee4533b786502
Evan Khosh <evank43@nycstudents.net>
2024-12-04 22:24:18 -0500

    Made border for screen

--- 2024-12-04-ColorScreen/Driver.java
+++ 2024-12-04-ColorScreen/Driver.java
@@ -1,5 +1,19 @@
 public class Driver {
   public static void main(String[] args){
-  
+    System.out.print(Text.CLEAR_SCREEN + Text.HIDE_CURSOR);
+    for (int i = 0; i < 80; i++){
+      for (int k = 0; k < 30; k++){
+        if (!(i == 0 || i == 79) && !(k == 0 || k == 29)){
+          k += 27;
+          continue;
+        }
+        Text.go(k+1, i+1);
+        Text.color(Text.WHITE, Text.background(Text.WHITE));
+        System.out.print(".");
+      }
+    }
+
+    Text.go(31, 1);
+    System.out.println(Text.RESET);
   }
 }
