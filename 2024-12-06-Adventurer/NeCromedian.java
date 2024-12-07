8a93e80823520e256c06c0521d50cbb3f1522285
Evan Khosh <evank43@nycstudents.net>
2024-12-07 10:46:32 -0500

    Wrote get methods and constructors for NeCromedian

--- 2024-12-06-Adventurer/NeCromedian.java
+++ 2024-12-06-Adventurer/NeCromedian.java
@@ -1,3 +1,31 @@
 public class NeCromedian extends Adventurer{
   private int special, specialMax;
+  
+  public NeCromedian(String name){
+    super(name, 10);
+    special = 50;
+    specialMax = 100;
+  }
+
+  public NeCromedian(String name, int hp){
+    super(name, hp);
+    special = 50;
+    specialMax = 100;
+  }
+
+  //give it a short name (fewer than 13 characters)
+  public String getSpecialName(){
+    return "Bad Jokes";
+  }
+
+  //accessor methods
+  public int getSpecial(){
+    return special;
+  }
+  public void setSpecial(int n){
+    special = n;
+  }
+  public int getSpecialMax(){
+    return specialMax;
+  }
 }
