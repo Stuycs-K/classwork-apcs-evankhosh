fbc9a62ed69ab8704e9ba4514230362cdf03ccd3
Evan <ekhosh60@stuy.edu>
2024-12-09 11:46:25 -0500

    Wrote code to instantiate CodeWarrior with user input

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Game {
   public static void main(String[] args){
-  
+    Scanner userInput = new Scanner(System.in);
+    String codeWarriorInput; String[] codeWarriorInputArr;
+
+    while (true){
+      System.out.println("Enter your Code Warrior's Name, HP, and Coding Language:");
+      codeWarriorInput = userInput.nextLine();
+      codeWarriorInputArr = String.split(codeWarriorInput);
+      try{
+        CodeWarrior(codeWarriorInputArr[0], Integer.parseInt(codeWarriorInputArr[1]), codeWarriorInputArr[2]);
+        break;
+      }
+      catch Exception ex{
+        System.out.println("Bad input. Try again.");
+        continue;
+      }
+    }
   }
 }
