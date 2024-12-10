bc32b986dd12e502b2ca38531cf575046231ae7f
Evan Khosh <evank43@nycstudents.net>
2024-12-09 19:57:12 -0500

    Wrote code to instantiate NeCromedian based on user input

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -3,20 +3,40 @@ import java.util.Scanner;
 public class Game {
   public static void main(String[] args){
     Scanner userInput = new Scanner(System.in);
-    String codeWarriorInput; String[] codeWarriorInputArr;
+    CodeWarrior codeWarrior; String codeWarriorInput; String[] codeWarriorInputArr;
+    NeCromedian neCromedian; String neCromedianInput; String[] neCromedianInputArr;
 
+    //Set up CodeWarrior
     while (true){
       System.out.println("Enter your Code Warrior's Name, HP, and Coding Language:");
       codeWarriorInput = userInput.nextLine();
-      codeWarriorInputArr = String.split(codeWarriorInput);
+      codeWarriorInputArr = codeWarriorInput.split(" ");
       try{
-        CodeWarrior(codeWarriorInputArr[0], Integer.parseInt(codeWarriorInputArr[1]), codeWarriorInputArr[2]);
+        codeWarrior = new CodeWarrior(codeWarriorInputArr[0], Integer.parseInt(codeWarriorInputArr[1]), codeWarriorInputArr[2]);
         break;
       }
-      catch Exception ex{
-        System.out.println("Bad input. Try again.");
+      catch (Exception ex){
+        System.out.println("Bad input. Try again with the format: \"Name HP(numbers only) Language\".");
         continue;
       }
     }
+
+    //Set up NeCromedian
+    while (true){
+      System.out.println("Enter your NeCromedian's Name and HP:");
+      neCromedianInput = userInput.nextLine();
+      neCromedianInputArr = neCromedianInput.split(" ");
+      try{
+        neCromedian = new NeCromedian(neCromedianInputArr[0], Integer.parseInt(neCromedianInputArr[1]));
+        break;
+      }
+      catch (Exception ex){
+        System.out.println("Bad input. Try again with the format: \"Name HP(numbers only)\".");
+        continue;
+      }
+    }
+
+    System.out.println(codeWarrior.getName() + ", " + codeWarrior.getHP() + "/" + codeWarrior.getmaxHP() + " HP, " + codeWarrior.getSpecial() + "/" + codeWarrior.getSpecialMax() + " " + codeWarrior.getSpecialName());
+    System.out.println(neCromedian.getName() + ", " + neCromedian.getHP() + "/" + neCromedian.getmaxHP() + " HP, " + neCromedian.getSpecial() + "/" + neCromedian.getSpecialMax() + " " + neCromedian.getSpecialName());
   }
 }
