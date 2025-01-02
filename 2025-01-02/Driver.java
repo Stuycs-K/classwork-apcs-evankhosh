d85d07bd2b01391e0dd39c06f2fec7e2d6cd6cba
Evan <ekhosh60@stuy.edu>
2025-01-02 11:47:08 -0500

    Wrote and tested fibbonacci sequence function

---
+++ 2025-01-02/Driver.java
@@ -0,0 +1,21 @@
+//Largest number that can be caluclated under 10s is 46
+//
+public class Driver{
+
+  public static void main(String[] args){
+    //System.out.println(fib(Integer.parseInt(args[0])));
+    for (int i = 0; i > -1; i++){
+      if (fib(i) < 0){
+        System.out.println(i-1);
+        break;
+      }
+    }
+  }
+
+  public static int fib(int n){
+    if (n < 2){
+      return n;
+    }
+    return fib(n-1) + fib(n-2);
+  }
+}
