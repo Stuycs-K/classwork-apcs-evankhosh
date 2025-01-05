c2e892ae99e0a5ce565d41698d34042f37fc4ad1
Evan Khosh <evank43@nycstudents.net>
2025-01-05 14:11:09 -0500

    Finished toWords function

--- 2025-01-03/Driver.java
+++ 2025-01-03/Driver.java
@@ -17,8 +17,11 @@ public class Driver{
   }
 
   public static String toWords(int n){
+    if (n < 0){
+      return "negative " + toWords(-n);
+    }
     if (n < 20){
-      if (n == 0) { return ""; }
+      if (n == 0) { return "zero"; }
       if (n == 1) { return "one"; }
       if (n == 2) { return "two"; }
       if (n == 3) { return "three"; }
@@ -39,7 +42,7 @@ public class Driver{
       if (n == 18) { return "eighteen"; }
       if (n == 19) { return "nineteen"; }
     }
-    if (n < 100){
+    if (n < 100 && n%10 > 0){
       if (n/10 == 2) {return "twenty-" + toWords(n%10); }
       if (n/10 == 3) {return "thirty-" + toWords(n%10); }
       if (n/10 == 4) {return "forty-" + toWords(n%10); }
@@ -49,5 +52,37 @@ public class Driver{
       if (n/10 == 8) {return "eighty-" + toWords(n%10); }
       if (n/10 == 9) {return "ninety-" + toWords(n%10); }
     }
+    if (n < 100){
+      if (n/10 == 2) {return "twenty"; }
+      if (n/10 == 3) {return "thirty"; }
+      if (n/10 == 4) {return "forty"; }
+      if (n/10 == 5) {return "fifty"; }
+      if (n/10 == 6) {return "sixty"; }
+      if (n/10 == 7) {return "seventy"; }
+      if (n/10 == 8) {return "eighty"; }
+      if (n/10 == 9) {return "ninety"; }
+    }
+    if (n < 1000){
+      if (n%100 > 0){
+        return toWords(n/100) + " hundred and " + toWords(n%100);
+      }
+      return toWords(n/100) + " hundred";
+    }
+    if (n < 1000000){
+      if (n%1000 > 0){
+        return toWords(n/1000) + " thousand " + toWords(n%1000);
+      }
+      return toWords(n/1000) + " thousand";
+    }
+    if (n < 1000000000){
+      if (n%1000000 > 0){
+        return toWords(n/1000000) + " million " + toWords(n%1000000);
+      }
+      return toWords(n/1000000) + " million";
+    }
+    if (n%1000000000 > 0){
+      return toWords(n/1000000000) + " billion " + toWords(n%1000000000);
+    }
+    return toWords(n/1000000000) + " billion";
   }
 }
